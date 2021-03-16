c0cf60eb993c3ee39b5e65efcb8672b5359a66d1
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-03-16 23:53:55 +0700

    Hoàn thành bài tập linearlayout bai 1 dotplays.com

---
+++ BaiTapLinearLayoutdotplayscomBai_1Lan_1/app/src/main/java/com/vientamthuong/baitaplinearlayout_dotplayscom_bai_1_lan_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vientamthuong.baitaplinearlayout_dotplayscom_bai_1_lan_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
