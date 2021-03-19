7ae6980d3b818aa6f44e337b7d1b13efac0a4473
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-03-19 23:47:15 +0700

    Làm được 1 ít bài nâng cao

---
+++ UIComponentExampleAdvanceLan_1/app/src/main/java/com/vientamthuong/uicomponentexampleadvance_lan_1/RegisterViewHolder.java
@@ -0,0 +1,16 @@
+package com.vientamthuong.uicomponentexampleadvance_lan_1;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RegisterViewHolder extends RecyclerView.ViewHolder {
+
+    //
+
+    public RegisterViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+}
