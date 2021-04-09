cae73161f827c04895b01c2dc40e27b08af3b97c
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-04-10 02:02:01 +0700

    Hoàn thành Volley String request

---
+++ VolleyAPINetworkRequestString/app/src/main/java/com/vientamthuong/volleyapinetworkrequeststring/MainActivity.java
@@ -0,0 +1,44 @@
+package com.vientamthuong.volleyapinetworkrequeststring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://vientamthuong.github.io/",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        textView.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText(error.toString());
+                    }
+                }
+        );
+        requestQueue.add(stringRequest);
+
+    }
+}
