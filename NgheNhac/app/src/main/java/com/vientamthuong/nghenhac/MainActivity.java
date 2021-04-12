1f28a8a133511f2be64d0a5aac9acc2dc34ed703
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-04-12 15:12:46 +0700

    Gan xong giao dien app nhac

---
+++ NgheNhac/app/src/main/java/com/vientamthuong/nghenhac/MainActivity.java
@@ -0,0 +1,17 @@
+package com.vientamthuong.nghenhac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Xóa status bar
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
