efd5f3b3d7b3b17b99b53de867a27eb5cbccce1f
vientamthuong <18130281@st.hcmuaf.edu.vn>
2021-05-29 10:18:53 +0700

    fragment replace

--- Fragment1/app/src/main/java/com/vientamthuong/fragment1/FragmentA.java
+++ Fragment1/app/src/main/java/com/vientamthuong/fragment1/FragmentA.java
@@ -13,7 +13,7 @@ public class FragmentA extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull  LayoutInflater inflater, @Nullable  ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_a, container, false);
     }
 }
--- Fragment1/app/src/main/java/com/vientamthuong/fragment1/MainActivity.java
+++ Fragment1/app/src/main/java/com/vientamthuong/fragment1/MainActivity.java
@@ -1,8 +1,12 @@
 package com.vientamthuong.fragment1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b1 = findViewById(R.id.b1);
+        Button b2 = findViewById(R.id.b2);
+
+        b1.setOnClickListener(v -> {
+            addFragment(new FragmentA());
+        });
+
+        b2.setOnClickListener(v -> {
+            addFragment(new FragmentB());
+        });
+    }
+
+    public void addFragment(Fragment f){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.f1,f);
+        fragmentTransaction.commit();
     }
 }
